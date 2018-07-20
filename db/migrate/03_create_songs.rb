88431b53d3309c7576241fb6d3cb6b0ad440dc4b
Christopher Wood <cjwood032@gmail.com>
2018-07-20 23:31:24 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,9 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
-    create_table :songs do |x|
-      x.string :name
-      x.integer :artist_id
-      x.integer :genre_id
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
     end
   end
 end
